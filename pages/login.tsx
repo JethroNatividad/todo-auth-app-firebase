c4498ead776614922d58f57698d3c6a29e1daa35
Jethro <kel73896@gmail.com>
2022-05-13 10:32:23 +0000

    login page done

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import type { NextPage } from 'next'
+import { Formik } from 'formik'
+
+type InitialValues = {
+  email:string
+  password: string
+}
 
 const Login:NextPage = () => {
+  const initialValues: InitialValues = {email:'', password:''}
   return (
-    <div>login</div>
+    <div>
+
+    <div className='text-center mb-2 text-xl uppercase font-semibold'>login</div>
+    <Formik initialValues={initialValues} onSubmit={(values, {setFieldValue})=> {
+      console.log(values)
+    }}>
+    {({
+         values,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form className='max-w-xl px-1 mx-auto flex flex-col space-y-2' onSubmit={handleSubmit}>
+           <input
+           className='outline-none border-2 border-black px-3 py-2 rounded-xl'
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+             placeholder='Email'
+           />
+          
+           <input
+           className='outline-none border-2 border-black px-3 py-2 rounded-xl'
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+             placeholder='Password'
+           />
+           
+           <button className='bg-black text-white px-3 py-2 rounded-xl' type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+    </Formik>
+    </div>
   )
 }
 
