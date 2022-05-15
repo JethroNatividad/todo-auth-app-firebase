da8e6adb4f28af46e561e79c879eeddd7806e3fa
Jethro <kel73896@gmail.com>
2022-05-15 08:47:32 +0000

    Add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const Navbar:NextPage = () => {
+  return (
+    <div className='shadow-sm h-12'>Navbar</div>
+  )
+}
+
+export default Navbar
