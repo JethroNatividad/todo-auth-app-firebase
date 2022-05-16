a4fd77a2ffd383d0bb715e101056acb2cd1cfb3e
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-16 18:30:12 +0000

    create todo component

---
+++ components/Todo.tsx
@@ -0,0 +1,11 @@
+import { Todo as TodoType } from "../types";
+
+const Todo = ({completed, text}:TodoType) => {
+    return (
+        <div>
+            Test
+        </div>
+    );
+}
+
+export default Todo;
