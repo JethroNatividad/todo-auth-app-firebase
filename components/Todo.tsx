aaadecac7728757ea0ca9cf93952db3c5b1c335a
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-16 18:41:00 +0000

    style todo

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -2,8 +2,8 @@ import { Todo as TodoType } from "../types";
 
 const Todo = ({completed, text}:TodoType) => {
     return (
-        <div>
-            Test
+        <div className="bg-gray-50 px-3 py-2 shadow-sm">
+            <p className={`${completed && 'line-through'}`}>{text}</p>
         </div>
     );
 }
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,8 +1,10 @@
+import Todo from './Todo'
+
 const NoteView = () => {
     return (
-        <div className="bg-gray-50 mx-4 my-3 shadow-md rounded-xl max-w-4xl px-2 py-1">
+        <div className="mx-4 my-3 shadow-md rounded-xl max-w-4xl space-y-5 pb-5">
           <h1 className="font-semibold text-center">TODOS</h1>
-          
+          <Todo completed={false} text={'todo'}/>
         </div>
     );
 }
