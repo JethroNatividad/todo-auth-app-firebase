02307b9c7900e1a1ceab81af48b0989d2a9ff8d8
Jethro <kel73896@gmail.com>
2022-05-17 06:23:19 +0000

    create todo function

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,8 @@
 import { collection, CollectionReference, doc, DocumentData, DocumentReference } from "firebase/firestore";
 import { db } from "./firebase";
 import { Todo, UserData } from "../types";
+import {addDoc} from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
 const createCollection = <T = DocumentData>(collectionName: string) => {
     return collection(db, collectionName) as CollectionReference<T>
   }
@@ -11,3 +13,10 @@ export const userRef = (uid:string) => {
     return doc(usersRef, uid)
 }
 export const todosRef = (uid:string) => createCollection<Todo>(`users/${uid}/todos`)
+
+export const createTodo = async ({completed, text}:Todo) => {
+  const auth = getAuth()
+  if(auth && auth.currentUser){
+    addDoc(todosRef(auth.currentUser.uid), {completed: false, text})
+  }
+}
