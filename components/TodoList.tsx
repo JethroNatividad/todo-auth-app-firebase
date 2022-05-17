f1d928716d58bc626291bada84aef42c12efbf09
Jethro <kel73896@gmail.com>
2022-05-17 11:59:43 +0000

    style todolist

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -15,11 +15,13 @@ const TodoList = ({todos}:Props) => {
     return (
         <div className="mx-4 my-3 shadow-md rounded-xl max-w-4xl space-y-5 pb-5">
           <h1 className="font-semibold text-center">TODOS</h1>
+          <div className='space-y-1'>
           {
             todos.map((todo) => 
-              <Todo completed={todo.completed} text={todo.text}/>
+            <Todo completed={todo.completed} text={todo.text}/>
             )
           }
+          </div>
           <CreateTodo/>
         </div>
     );
