0d7940e72244e81bddb5c263b585fdd24a012474
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 09:09:52 +0000

    break word

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -3,7 +3,11 @@ import { Todo as TodoType } from "../types";
 const Todo = ({ completed, text }: TodoType) => {
     return (
         <div className="bg-gray-50 px-3 py-2 shadow-sm hover:shadow-md transition-all ease-in-out">
-            <p className={`${completed && 'line-through'}`}>{text}</p>
+            <div className="flex">
+                <div className={`${completed && 'line-through'} flex-1 break-all`}>{text}</div>
+                <div>edit</div>
+            </div>
+
         </div>
     );
 }
