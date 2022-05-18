fdc1d0fbb489318731ddeae9ab2ee325e24af8f8
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 09:50:27 +0000

    implement editTodo

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,33 @@
-import { collection, CollectionReference, doc, DocumentData, DocumentReference } from "firebase/firestore";
+import { collection, CollectionReference, doc, DocumentData, DocumentReference, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import { Todo, UserData } from "../types";
-import {addDoc} from 'firebase/firestore'
+import { addDoc } from 'firebase/firestore'
 import { getAuth } from "firebase/auth";
 const createCollection = <T = DocumentData>(collectionName: string) => {
-    return collection(db, collectionName) as CollectionReference<T>
-  }
-  
+  return collection(db, collectionName) as CollectionReference<T>
+}
+
 
 export const usersRef = createCollection<UserData>('users')
-export const userRef = (uid:string) => {
-    return doc(usersRef, uid)
+export const userRef = (uid: string) => {
+  return doc(usersRef, uid)
 }
-export const todosRef = (uid:string) => createCollection<Todo>(`users/${uid}/todos`)
+export const todosRef = (uid: string) => createCollection<Todo>(`users/${uid}/todos`)
 
-export const createTodo = async ({completed, text}:Todo) => {
+export const todoRef = (todosRef: CollectionReference<Todo>, id: string) => {
+  return doc(todosRef, id)
+}
+
+export const createTodo = async ({ completed, text }: Todo) => {
   const auth = getAuth()
-  if(auth && auth.currentUser){
-    addDoc(todosRef(auth.currentUser.uid), {completed, text})
+  if (auth && auth.currentUser) {
+    addDoc(todosRef(auth.currentUser.uid), { completed, text })
+  }
+}
+export const editTodo = async (id: string, { completed, text }: Todo) => {
+  const auth = getAuth()
+  if (auth && auth.currentUser) {
+    const refTodos = todosRef(auth.currentUser.uid)
+    setDoc(todoRef(refTodos, id), { completed, text })
   }
 }
