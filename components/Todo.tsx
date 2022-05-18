bde6a7014e4adaca8ab106ac086660ff5ae4d06d
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 09:44:45 +0000

    Add edit state and styled

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,14 +1,58 @@
-import { Todo as TodoType } from "../types";
+import { Formik } from "formik";
+import { useState } from "react";
+import { TodoFront } from "../types";
 
-const Todo = ({ completed, text }: TodoType) => {
+const Todo = ({ completed, text, id }: TodoFront) => {
+    const [editing, setEditing] = useState<boolean>(false)
     return (
         <div className="bg-gray-50 px-3 py-2 shadow-sm hover:shadow-md transition-all ease-in-out">
-            <div className="flex">
-                <div className={`${completed && 'line-through'} flex-1 break-all`}>{text}</div>
-                <div>edit</div>
-            </div>
+
+            {
+                editing
+                    ? (
+                        <Formik initialValues={{ text }} onSubmit={async ({ text }, { setSubmitting, setFieldValue }) => {
+                            setSubmitting(true)
+                            console.log(text)
+                            setFieldValue('text', text);
+                            // await createTodo({ text, completed: false })
+                            setSubmitting(false)
+                            setEditing(false)
+                        }}>
+                            {({
+                                values,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                                /* and other goodies */
+                            }) => (
+                                <form className="flex space-x-1 w-full " onSubmit={handleSubmit}>
+                                    <input
+                                        className='outline-none flex-1 border-2 border-black px-3 py-2 rounded-xl'
+                                        type="text"
+                                        name="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.text}
+                                        placeholder='Type something...'
+                                    />
+                                    <button disabled={isSubmitting} className="border-2 rounded-xl border-black px-2 py-1" type="submit">Save</button>
+                                </form>
+                            )}
+                        </Formik>
+                    )
+                    : (
+                        <div className="flex group items-center">
+                            <div className={`${completed && 'line-through'} flex-1 break-all`}>{text}</div>
+                            <div className='invisible group-hover:visible'>
+                                <button onClick={() => setEditing(true)} className="border-2 rounded-xl border-black px-2 py-1" type="button">Edit</button>
+                            </div>
+                        </div>)
+            }
+
 
         </div>
+
     );
 }
 
