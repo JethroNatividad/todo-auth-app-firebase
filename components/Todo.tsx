61179d081d2455e3fa8d4a78b147acf2ee8ea535
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 09:02:07 +0000

    onhover shadow

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,8 +1,8 @@
 import { Todo as TodoType } from "../types";
 
-const Todo = ({completed, text}:TodoType) => {
+const Todo = ({ completed, text }: TodoType) => {
     return (
-        <div className="bg-gray-50 px-3 py-2 shadow-sm">
+        <div className="bg-gray-50 px-3 py-2 shadow-sm hover:shadow-md transition-all ease-in-out">
             <p className={`${completed && 'line-through'}`}>{text}</p>
         </div>
     );
