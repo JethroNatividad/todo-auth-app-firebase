f8d1169269d76dae3097cfd213d78635bf17c339
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 13:36:54 +0000

    Add hover effects btn login

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,74 +5,74 @@ import { sign } from 'crypto'
 import { login } from '../lib/auth'
 import { useRouter } from 'next/router'
 import { onAuthStateChanged } from 'firebase/auth'
-import {auth} from '../lib/firebase'
+import { auth } from '../lib/firebase'
 type InitialValues = {
-  email:string
+  email: string
   password: string
 }
 
-const Login:NextPage = () => {
+const Login: NextPage = () => {
   const router = useRouter()
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if(user) return router.push('/')
+      if (user) return router.push('/')
     })
     return () => {
       unsubscribe()
     }
   }, [])
-  
-  const initialValues: InitialValues = {email:'', password:''}
+
+  const initialValues: InitialValues = { email: '', password: '' }
   return (
     <div>
 
-    <div className='text-center mb-2 text-xl uppercase font-semibold'>login</div>
-    <Formik initialValues={initialValues} onSubmit={async (values, {setFieldValue, setSubmitting})=> {
-      console.log(values)
-      setSubmitting(true)
-      const [error, data] = await login(values.email,values.password)
-      setSubmitting(false)
-      if(error) return alert(error)
-      router.push('/')
-    }}>
-    {({
-         values,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-         <form className='max-w-xl px-1 mx-auto flex flex-col space-y-2' onSubmit={handleSubmit}>
-           <input
-           className='outline-none border-2 border-black px-3 py-2 rounded-xl'
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-             placeholder='Email'
-           />
-          
-           <input
-           className='outline-none border-2 border-black px-3 py-2 rounded-xl'
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-             placeholder='Password'
-           />
-           
-           <button className='bg-black text-white px-3 py-2 rounded-xl' type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-           <button onClick={()=>router.push('/signup')} className='bg-black text-white px-3 py-2 rounded-xl' type="button" disabled={isSubmitting}>
-             Signup
-           </button>
-         </form>
-       )}
-    </Formik>
+      <div className='text-center mb-2 text-xl uppercase font-semibold'>login</div>
+      <Formik initialValues={initialValues} onSubmit={async (values, { setFieldValue, setSubmitting }) => {
+        console.log(values)
+        setSubmitting(true)
+        const [error, data] = await login(values.email, values.password)
+        setSubmitting(false)
+        if (error) return alert(error)
+        router.push('/')
+      }}>
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form className='max-w-xl px-1 mx-auto flex flex-col space-y-2 ' onSubmit={handleSubmit}>
+            <input
+              className='outline-none border-2 border-black px-3 py-2 rounded-xl'
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              placeholder='Email'
+            />
+
+            <input
+              className='outline-none border-2 border-black px-3 py-2 rounded-xl'
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              placeholder='Password'
+            />
+
+            <button className='bg-black  text-white px-3 py-2 rounded-xl border-2 hover:bg-white hover:text-black hover:border-black transition-all ease-in-out' type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+            <button onClick={() => router.push('/signup')} className='bg-black text-white px-5 py-2 rounded-xl border-2 hover:bg-white hover:text-black hover:border-black transition-all ease-in-out' type="button" disabled={isSubmitting}>
+              Create an account
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   )
 }
