8e5ceba96f992d6fc36109ab22be454018ae19c6
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-18 13:36:47 +0000

    Add hover effects btn nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,11 @@ type Props = {
   signout: () => void
 }
 
-const Navbar = ({username, loading, signout}:Props) => {
+const Navbar = ({ username, loading, signout }: Props) => {
   return (
     <div className='shadow-sm h-12 flex items-center justify-between px-5'>
-      <h1 className='uppercase font-semibold'>{loading ? 'loading' :`Welcome, ${username}`}</h1>
-      <button className='px-2 py-1 border-2 border-black rounded-xl' onClick={signout} disabled={loading}>{loading ? 'loading' : 'Logout'}</button>
+      <h1 className='uppercase font-semibold'>{loading ? 'loading' : `Welcome, ${username}`}</h1>
+      <button className='px-2 py-1 border-2 border-black rounded-xl hover:bg-black hover:text-white transition-all ease-in-out' onClick={signout} disabled={loading}>{loading ? 'loading' : 'Logout'}</button>
     </div>
   )
 }
