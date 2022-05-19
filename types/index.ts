44a18e67c615330ab3207aca704f9fc67137c74f
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-19 16:08:51 +0000

    add timestamp in types

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,16 @@
+import { FieldValue } from "firebase/firestore"
+
 export type Todo = {
     text: string
     completed: boolean
+    timestamp?: FieldValue
 }
 
 export type UserData = {
     username: string
     email: string
+    timestamp?: FieldValue
+
 }
 
 export interface TodoFront extends Todo {
