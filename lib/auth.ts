07f0c89a714295f73d5a6320b1aba303d65e9736
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-05-19 15:44:28 +0000

    fix signup bug

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,20 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "./firebase";
-import { userRef} from './db'
+import { userRef } from './db'
 import { getDoc, setDoc } from "firebase/firestore";
 import { getErrorMessage } from "./utils";
 
-export async function getUserData(uid:string) {
+export async function getUserData(uid: string) {
   try {
     const user = await getDoc(userRef(uid))
-    if(!user.exists() || user.data() === undefined) throw new Error('User doesnt exist')
+    if (!user.exists() || user.data() === undefined) throw new Error('User doesnt exist')
     return user.data()
-  } catch (error:unknown) {
-    const message:string = getErrorMessage(error)
+  } catch (error: unknown) {
+    const message: string = getErrorMessage(error)
     throw new Error(message)
   }
 }
-export async function signup(username:string, email:string, password:string){
+export async function signup(username: string, email: string, password: string) {
   try {
     const data = await createUserWithEmailAndPassword(auth, email, password)
     const { user } = data
@@ -23,24 +23,24 @@ export async function signup(username:string, email:string, password:string){
       email,
       username
     })
-    const userData = await getUserData(user.uid)
-    
-    return [null, userData, user.uid]
+
+
+    return [null, { email, username }, user.uid]
   } catch (error: unknown) {
-    const message:string = getErrorMessage(error)
+    const message: string = getErrorMessage(error)
     return [message, null, null]
   }
 
 }
-export async function login(email:string, password:string){
+export async function login(email: string, password: string) {
   try {
     const data = await signInWithEmailAndPassword(auth, email, password)
-    const {user} = data
+    const { user } = data
     console.log(data)
     const userData = await getUserData(user.uid)
     return [null, userData, user.uid]
-  } catch (error:unknown) {
-    const message:string = getErrorMessage(error)
+  } catch (error: unknown) {
+    const message: string = getErrorMessage(error)
     return [message, null, null]
   }
 }
